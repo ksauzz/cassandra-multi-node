b824e3ba6e375e39725dc3e60a4233841f015d29
ksauzz <ksauzzmsg@gmail.com>
2012-08-20 14:59:04 +0900

    copy entire package.

--- multi-node.sh
+++ multi-node.sh
@@ -6,31 +6,34 @@ usage() {
   cat<<EOS
 usage
 -----
-  $0 create node_count
+  $0 create node_count cassandra_home
   $0 clean
 EOS
 }
 
 clean(){
-  rm -rfv $ROOT/nodes
+  rm -rf $ROOT/nodes
 }
 
 create_nodes(){
   node_cnt=$(expr $1)
-  i=$node_cnt
+
+  cassandra_home=$2
 
   n=1
   while [ $n -le $node_cnt ]; do seeds="$seeds,127.0.0.$n";n=$(($n+1)); done
   seeds=$(echo $seeds|sed s/,//)
 
+  i=$node_cnt
   while [ $i -gt 0 ]; do
     app_root=$ROOT/nodes/$i
-    mkdir -p $app_root/conf
-    mkdir -p $app_root/data
-    mkdir -p $app_root/commitlog
-    mkdir -p $app_root/saved_caches
+    mkdir -pv $app_root
+    cp -r $cassandra_home/* $app_root/
+    mkdir -pv $app_root/data
+    mkdir -pv $app_root/commitlog
+    mkdir -pv $app_root/saved_caches
 
-    cat $ROOT/etc/cassandra.yaml | \
+    cat $cassandra_home/conf/cassandra.yaml | \
       sed "s|/var/lib/cassandra|${app_root}|" |\
       sed "s|: localhost|: 127.0.0.$i|" |\
       sed "s|- seeds: \"127.0.0.1\"|- seeds: \"$seeds\"|" \
@@ -47,7 +50,11 @@ fi
 
 case $1 in
   create)
-    create_nodes $2
+    if [[ $# -lt 3 ]]; then
+      usage
+      exit 1
+    fi
+    create_nodes $2 $3
     ;;
   clean)
     clean
